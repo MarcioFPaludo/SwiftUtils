e522a7b9d0a043f33fc57ab196da019598259e68
Marcio F. Paludo <marciof.paludo@gmail.com>
2021-07-25 16:38:12 -0300

    Add Header and Footer fit to size

--- Sources/SwiftUtils/Extensions/UITableView+Utils.swift
+++ Sources/SwiftUtils/Extensions/UITableView+Utils.swift
@@ -8,6 +8,36 @@
 import UIKit
 
 extension UITableView {
+    public func sizeHeaderToFit() {
+        guard let headerView = tableHeaderView else { return }
+
+        let height = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+
+        var frame = headerView.frame
+        
+        // avoids infinite loop!
+        if height != frame.height {
+            frame.size.height = height
+            headerView.frame = frame
+            tableHeaderView = headerView
+        }
+    }
+    
+    public func sizeFooterToFit() {
+        guard let footerView = tableFooterView else { return }
+
+        let height = footerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+
+        var frame = footerView.frame
+        
+        // avoids infinite loop!
+        if height != frame.height {
+            frame.size.height = height
+            footerView.frame = frame
+            tableFooterView = footerView
+        }
+    }
+    
     public func dequeueReusableCell<T: UITableViewCell & IdentifiableProtocol>(with type: T.Type, for indexPath: IndexPath) -> T {
         return dequeueReusableCell(withIdentifier: T.identifier, for: indexPath) as! T
     }
